596b48d6fedd31bc99961c86f9687f239497591d
Dennis Fricke <defri001@stud.fh-dortmund.de>
2018-02-11 17:17:37 +0100

    + added Spring initialization

---
+++ src/main/java/io/freefair/javafxdi/JavaFXHelper.java
@@ -0,0 +1,57 @@
+package io.freefair.javafxdi;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Callback;
+
+import java.net.URL;
+
+public class JavaFXHelper {
+
+	private static FXMLLoader loader() {
+		return loader(new FXMLLoader());
+	}
+
+	private static FXMLLoader loader(URL url) {
+		return loader(new FXMLLoader(url));
+	}
+
+	private static FXMLLoader loader(FXMLLoader loader) {
+		loader.setControllerFactory(param -> Application.getInstance().getContext().getBean(param));
+		return loader;
+	}
+
+	public static Parent loadFile(String filename) {
+		try {
+			return loader().load(Thread.currentThread().getContextClassLoader().getResourceAsStream(filename));
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	public static void loadAndSet(String filename, AnchorPane controlNode) {
+		FXMLLoader fxmlLoader = loader(Thread.currentThread().getContextClassLoader().getResource(filename));
+		fxmlLoader.setRoot(controlNode);
+		fxmlLoader.setController(controlNode);
+		try {
+			fxmlLoader.load();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	public static void addToAnchorFull(AnchorPane pane, Node child) {
+		pane.getChildren().add(child);
+		AnchorPane.setBottomAnchor(child, 0.0);
+		AnchorPane.setTopAnchor(child, 0.0);
+		AnchorPane.setLeftAnchor(child, 0.0);
+		AnchorPane.setRightAnchor(child, 0.0);
+	}
+
+	public static void replaceRoot(AnchorPane rootPane, String s) {
+		rootPane.getChildren().clear();
+		addToAnchorFull(rootPane, loadFile(s));
+	}
+}
